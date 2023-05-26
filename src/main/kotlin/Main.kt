b77c5868d15b47b8fb16c76d5112060c40141624
PcAudio <byandersj@gmail.com>
2023-05-26 11:39:05 -0500

    Kotlin y sus tipos de variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -18,6 +18,67 @@ fun main(args: Array<String>) {
 
     println(PI)
 
+    val boolean = true
+
+    val numeroLargo:Long = 300000000000000
+
+    val numeroLargoDos = 3L
+
+    val double :Double =2.7182
+    val double2 =2.7182
+
+    val float : Float = 1.1f
+    val float2 = 1.1f
+
+
+    val primerValor =20
+
+    println("-------------")
+
+    println( primerValor.plus(30))
+
+
+    val segundoValor=10
+
+    //si me paro en cima de minux y le doy contrl b voy a la declaracion de la funcion
+
+    val tercerValor =primerValor.minus(segundoValor)
+
+    println(tercerValor)
+
+    println("-------------")
+
+    val cuartoValor =primerValor - segundoValor
+
+    println(cuartoValor)
+
+    println("-------------")
+
+    val apellidoo="APELLIDO"
+
+    val nombree="NOMBRE"
+
+    val nombreCompleto = nombree + " "+ apellidoo
+    println(nombreCompleto)
+    println("-------------")
+
+    val nombreCompletooo = "$nombree $apellidoo"
+
+    println(nombreCompletooo)
+
+    val nombreDeOTraForma = "Mi nombre es $nombree"
+
+    println(nombreDeOTraForma)
+
+
+
+
+
+
+
+
+
+
 
 
     // Try adding program arguments via Run/Debug configuration.
