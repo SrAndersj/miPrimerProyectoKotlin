231a14f82e31c87412dc6e6a0028d49995f0fa9d
PcAudio <byandersj@gmail.com>
2023-05-28 10:06:45 -0500

    20 Elvis operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,17 @@
 fun main(args: Array<String>) {
-    //? significa que es nulable
-    var nombre: String? =null
+    //convertir un nulable a un no nulable
 
-    // esto es lo que debemos evitar
-    //!! le estoy diciendo a kotlin que estoy 100% seguro que no es un valor nulo
-    // nombre!!.length
+    var nombre:String?=null
 
+   //operador elvis ?: asigna un valor por defecto
+    //en el momento que una propiedad sea nula
+    val caracterDeNombre:Int=nombre?.length?:0
 
-    nombre?.length
-    println(nombre?.length)
-
-    println("-------------")
-
-    try {
-
-        nombre!!.length
-
-    }catch (exception : NullPointerException){
-        println("ha ocurrido un error")
-    }finally {
-        println("ha courrido un error cerrando aplicacion ")
-    }
+    println(caracterDeNombre)
 
 
-    val primerValor = 10
-    val segundoValor=0
-
-    val resultado : Int =try{primerValor/segundoValor}catch (exception:Exception){0}
-    println(resultado)
 
     }
 
 
-    //recomendacion evitar usar !!
+
