1ad0c23cc50d43abcc79574c03077496adc71896
PcAudio <byandersj@gmail.com>
2023-05-30 22:21:36 -0500

    21 Listas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,49 @@
 fun main(args: Array<String>) {
-    //convertir un nulable a un no nulable
 
-    var nombre:String?=null
+    val listaDeNombres = listOf("Juan","Enrique","Camila")
 
-   //operador elvis ?: asigna un valor por defecto
-    //en el momento que una propiedad sea nula
-    val caracterDeNombre:Int=nombre?.length?:0
+    println(listaDeNombres)
 
-    println(caracterDeNombre)
+    //lista mutable
 
+    val listaVacia = mutableListOf<String>()
+
+    println(listaVacia)
+
+    listaVacia.add("Andres")
+
+    println(listaVacia)
+
+
+    val valorUsandoGet=listaVacia.get(0)
+
+    println(valorUsandoGet)
+
+    val valorUsandoOperador= listaVacia[0]
+    println(valorUsandoOperador)
+
+
+    val primerValor = listaDeNombres.first()
+
+    println(primerValor)
+
+    // val primerValor = listaDeNombres.firstOrNull()
+
+    listaVacia.removeAt(0)
+
+    println(listaVacia)
+
+    listaVacia.add("Andres")
+
+    listaVacia.removeIf { caracteres -> caracteres.length > 3 }
+
+    println(listaVacia)
+
+
+    val array = arrayOf(1,2,3,4)
+
+    println("nuestro array $array")
+    println("nuestro array como lista ${array.toList()}")
 
 
     }
