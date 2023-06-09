e3bc5b91454fe1956b36241edcbb224643926f74
PcAudio <byandersj@gmail.com>
2023-06-08 20:00:56 -0500

    27 Tipos de parámetros en las funciones

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,56 +1,20 @@
-@file:Suppress("UNREACHABLE_CODE")
-
 fun main(args: Array<String>) {
 
-    val fraseAleatoria="En Platzi nunca paramos de aprender"
+    imprimirNombre(nombre="Giuseppe",apellido="Vetri")
 
-    imprimirFrase(fraseAleatoria)
-
-    val fraseOrdenadaAleatoriamente = randomCase(fraseAleatoria)
-
-    println(fraseOrdenadaAleatoriamente)
-
-    val fraseAleatoriaExtension ="En Platzi nunca paramos de aprender frase por extension".randomCaseExtension()
-
-    println(fraseAleatoriaExtension)
-}
-
-
-
-
-
-
-fun imprimirFrase(frase:String):Unit{
-
-    println("tu rase es : $frase")
-}
-
-fun randomCase(frase:String):String {
-
-    val numeroAleatorio = 0..99
-    val resultadoAleatorio = numeroAleatorio.random()
-    //rem es para devolver el residuo de la division del numero dividido 2
-    return if (resultadoAleatorio.rem(2) == 0) {
-        return frase.toUpperCase()
-
-    } else {
-        frase.toLowerCase()
-    }
 
 }
 
-    fun String.randomCaseExtension(): String {
+fun imprimirNombre(nombre:String,segundoNombre:String="",apellido:String){
 
-        val numeroAleatorioExtension = 0..99
-        val resultadoAleatorioExtension = numeroAleatorioExtension.random()
-        //rem es para devolver el residuo de la division del numero dividido 2
-        return if (resultadoAleatorioExtension.rem(2) == 0) {
-            return this.toUpperCase()
+    println("Mi nombre es $nombre y mi apellido es $apellido")
 
-        } else {
-            this.toLowerCase()
-        }
-    }
+    println("mi nombtre completo es $nombre $apellido")
+
+    println("mi nombtre completo es $nombre $segundoNombre $apellido")
+}
+
+
 
 
 
