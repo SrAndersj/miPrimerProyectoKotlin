05603436365039f6caa0fc8bfcd6b01cfe478b8c
PcAudio <byandersj@gmail.com>
2023-06-30 14:19:18 -0500

    29 High Order functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,32 @@
+//block es un sintaxis recomendada por kotlin para nombrar las lambdas
 fun main(args: Array<String>) {
 
-    val myLambda : (String) -> Int={valor -> valor.length }
-    val lambdaEjecutada:Int = myLambda("Hola Platzi")
-    println(lambdaEjecutada)
+    val largoDelValorInicial = superFuncion(valorInicial = "Hola") { valor ->
+        valor.length
+    }
 
-    val saludos = listOf("Hello","hola","ciao")
-    val longitudSaludos = saludos.map(myLambda)
-    println(longitudSaludos)
+    println(largoDelValorInicial)
+
+    val lambda: () -> String = funcionInception("Enrique")
+
+    val valorLambda =lambda()
+
+    println(valorLambda)
+
 }
 
+// esta lambda recibe un string y devuelve un entero
+fun superFuncion(valorInicial: String,block: (String)->Int):Int{
 
+    return block(valorInicial)
+}
+
+fun funcionInception(nombre:String):()-> String{
+
+    return {
+        "Hola desde la lambda $nombre"
+    }
+}
 
 
 
