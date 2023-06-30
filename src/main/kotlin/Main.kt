096b0eacc990c31c76e52698f484138e58ea10cd
PcAudio <byandersj@gmail.com>
2023-06-30 14:24:19 -0500

    30 Let

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,20 @@
-//block es un sintaxis recomendada por kotlin para nombrar las lambdas
+
 fun main(args: Array<String>) {
 
-    val largoDelValorInicial = superFuncion(valorInicial = "Hola") { valor ->
-        valor.length
+    var nombre : String? =null
+
+    nombre?.let{
+        valor -> println("el nombre no es nulo es $valor")
+    }
+    nombre="Giuseppe"
+
+    nombre?.let{
+        valor -> println("El nombre no es nulo es realmente $valor")
     }
 
-    println(largoDelValorInicial)
-
-    val lambda: () -> String = funcionInception("Enrique")
-
-    val valorLambda =lambda()
-
-    println(valorLambda)
-
 }
 
-// esta lambda recibe un string y devuelve un entero
-fun superFuncion(valorInicial: String,block: (String)->Int):Int{
 
-    return block(valorInicial)
-}
-
-fun funcionInception(nombre:String):()-> String{
-
-    return {
-        "Hola desde la lambda $nombre"
-    }
-}
 
 
 
