ffbe5e58e549094ef5b895e21bb6648c3f47f522
PcAudio <byandersj@gmail.com>
2023-07-30 09:44:10 -0500

    33 Apply , nos permite realizar operaciones sobre una variable y luego devolver el valor al contrario de run que toca devolver el this

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
 
 fun main(args: Array<String>) {
 
-    val moviles = mutableListOf("Google Pixel 2xl","Google Pixel 4a ","Huawei Redmi 9","xiaomi mi a3")
-        moviles.run {
-            removeIf{ movil -> movil.contains("Google")}
+    val moviles = mutableListOf("Google Pixel 2xl","Google Pixel 4a ","Huawei Redmi 9","xiaomi mi a3").apply{
+        removeIf {movil -> movil.contains("Google")}
         }
+        println(moviles)
 
-    println(moviles)
 
-
+        val colores :MutableList<String>?= mutableListOf("Amarillo","azul","Rojo")
+        colores?.apply{
+            println("nuestros colores son $this")
+            println("la cantidd de colres es $size")
+        }
 }
 
 
