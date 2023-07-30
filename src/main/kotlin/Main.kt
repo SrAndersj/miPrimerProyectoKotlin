cde9a500e7f35243baca0da3a0c9ae66fdebc7b0
PcAudio <byandersj@gmail.com>
2023-07-30 09:49:06 -0500

    34 Also , nos permite obtener una variable , luego ejecutar un codigo con esa variable y devolverla como parametro para poderla usar como parametro en otra funcion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,14 @@
 
 fun main(args: Array<String>) {
 
-    val moviles = mutableListOf("Google Pixel 2xl","Google Pixel 4a ","Huawei Redmi 9","xiaomi mi a3").apply{
-        removeIf {movil -> movil.contains("Google")}
-        }
+    val moviles = mutableListOf("Google Pixel 2xl","Google Pixel 4a ","Huawei Redmi 9","xiaomi mi a3")
+        .also{
+            lista -> println("el valor original de la lista es $lista")
+
+        }.asReversed()
+        println(" ")
         println(moviles)
 
-
-        val colores :MutableList<String>?= mutableListOf("Amarillo","azul","Rojo")
-        colores?.apply{
-            println("nuestros colores son $this")
-            println("la cantidd de colres es $size")
-        }
 }
 
 
